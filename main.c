5bcc8e0412798fb2d9f247cdba0d7fe68240fbda
ssv32 <ssv_32@mail.ru>
2015-08-14 23:40:42 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+ * GccApplication5.c
+ *
+ * Created: 02.08.2015 11:01:02
+ *  Author: ssv
+ */ 
+
 #define F_CPU 4000000L
 #include <avr/io.h>
 #include <util/delay.h>
@@ -8,23 +15,37 @@ int main(void)
 	DDRB=0b00000000;
 	
 	PORTD=0b11111111;
-	_delay_ms(500);	
-  PORTD=0b00000000;
-	
+	_delay_ms(100);	
+	PORTD=0b00000000;
+    int NButton=0;  //number button
+	// 0  1  2  3    4    5    6    7
+	// +  -  *  /  enter  Up  Down  clear
+	int TimerButton=0; //TimerButton
+		
+		
     while(1)
     {
+	PORTD=0b00000000;
+		if (PINB==0b00000010) {
+			NButton=1;
+			TimerButton++;
+			if(TimerButton>10){
+				PORTD=0b00000010;
+				TimerButton=0;
+			}
+		} else if (NButton==1) TimerButton=0;
 		
-		if (PINB==0b00001000)   {	
-			a=a-200;
-			if (a<0) a=1000;
-		}
-		PORTD=0b11111111;
-		_delay_ms(a);
-		PORTD=0b00000000;
-		_delay_ms(a);
+		if (PINB==0b00000001) {
+			NButton=2;
+			TimerButton++;
+			if(TimerButton>10){
+				PORTD=0b00000001;
+				TimerButton=0;
+			}
+		} else if (NButton==2) TimerButton=0;
+		
 		
         //TODO:: Please write your application code 
     }
-    
 	return 0;
 }
